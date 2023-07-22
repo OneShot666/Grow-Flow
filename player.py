fc628e8feedbe9d6b6491c75a10b57bad11a4d87
Nathan <69707804+OneShot666@users.noreply.github.com>
2023-07-22 16:09:37 +1100

    v1.0.0

--- player.py
+++ player.py
@@ -1,88 +1,128 @@
 from math import sqrt
 # from random import *
 # from time import *
-from bubble import Bubble
+from bubble import Bubble, CellEaten
 import pygame
 
 
-# ! [later] Upgrade functions Eat() + Attack()
-# ! [later] Add function Evolve() (modify appearance + lenght of player)
+# ! [later] Upgrade function Attack()
+# ! [later] Add function Evolve() (modify appearance)
 class Player(Bubble):                                                           # Player in the game
-    def __init__(self, x=0, y=0, map_borders=None, size=100, color=(216, 216, 216), name="Player", bg_size=None,
-                 length=1, speed=20, *groups):
+    def __init__(self, x=0, y=0, map_borders=None, screen_size=None, size=100, color=(216, 216, 216),
+                 name="Player", length=1, speed=20, *groups):
         super().__init__(x, y, map_borders, size, color, speed, *groups)
+        self.is_dead = False
         self.name = name
         self.group = groups
         self.Methods = ["mouse", "keyboard"]
         self.move_method = self.Methods[0]                                      # Mouse movement by default
-        self.evolution = 0                                                      # ! [later]
+        self.screen_size = screen_size
+        self.evolution = 0                                                      # ! [later] Appearance
         self.length = length                                                    # ! [later]
+
+        self.max_diameter = size
+        self.max_radius = int(size * 0.5)
+        self.current_radius = int(self.max_radius * 0.5)
+        self.current_diameter = self.current_radius * 2
+        self.screen_pos = (int(self.screen_size[0] * 0.5 - self.current_radius), int(self.screen_size[1] * 0.5 - self.current_radius))
+
         self.life = 100
         self.life_max = 100
         self.cells_eaten = 0
-        self.cells_eaten_max = 0                                                # Depends on nb of cells in level
+        self.cells_eaten_max = 1                                                # Depends on nb of cells in level
+        self.stomach_capacity = 10
+        self.Stomach = []                                                       # [later] Use as money ?
+
         self.life_color = (45, 240, 115)
         self.life_color_bg = (30, 160, 75)
         self.cell_color = (255, 230, 25)
         self.cell_color_bg = (255, 250, 225)
 
-        self.membrane_width = int(self.diameter * 0.05)                         # Cellular membrane of player
+        self.membrane_width = int(self.current_radius * 0.1)                    # Cellular membrane of player
         self.image = None
         self.reflect_surface = None
-        self.set_image()
+        self.set_image(self.current_radius)
         self.rect = self.image.get_rect()
         self.rect.x = self.set_position_x() if x == 0 else x
         self.rect.y = self.set_position_y() if y == 0 else y
 
-    @staticmethod
-    def setName(name):                                                          # Set player name ([later] move in menu)
-        if name in ["Player", "", None]:
-            name = input("Entrez votre nom : ")
-        return name
+    def setName(self, name=None):                                                     # Set player name
+        self.name = "Player" if name is None else name
 
     def set_position_x(self):                                                   # Put player in middle of map
-        return int(self.map_borders[0] + self.map_borders[2] * 0.5 - self.radius)
+        return int(self.map_borders[0] + self.map_borders[2] * 0.5 - self.current_radius)
 
-    def set_position_y(self):                                                   # Same
-        return int(self.map_borders[1] + self.map_borders[3] * 0.5 - self.radius)
+    def set_position_y(self):                                                   # //
+        return int(self.map_borders[1] + self.map_borders[3] * 0.5 - self.current_radius)
 
-    def set_image(self):
-        self.image = pygame.Surface((self.diameter, self.diameter), pygame.SRCALPHA)
-        pygame.draw.circle(self.image, self.color, (self.radius, self.radius), self.radius)
-        pygame.draw.circle(self.image, self.color_border, (self.radius, self.radius), self.radius, self.membrane_width)
-        self.reflect_surface = pygame.Surface((int(self.diameter * 0.15), int(self.diameter * 0.3)), pygame.SRCALPHA)
+    def set_image(self, radius=None):
+        radius = self.current_radius if radius is None else radius
+        self.image = pygame.Surface((radius * 2, radius * 2), pygame.SRCALPHA)
+        pygame.draw.circle(self.image, self.color, (radius, radius), radius)
+        pygame.draw.circle(self.image, self.color_border, (radius, radius), radius, self.membrane_width)
+        self.reflect_surface = pygame.Surface((int(radius * 0.3), int(radius * 0.6)), pygame.SRCALPHA)
         pygame.draw.ellipse(self.reflect_surface, self.color_reflect,
                             (0, 0, self.reflect_surface.get_width(), self.reflect_surface.get_height()))
-        self.reflect_surface = pygame.transform.rotate(self.reflect_surface, -45)                       # Rotate surface
-        self.image.blit(self.reflect_surface, (self.radius * 0.3, self.radius * 0.3))
+        self.reflect_surface = pygame.transform.rotate(self.reflect_surface, -45)   # Rotate surface
+        self.image.blit(self.reflect_surface, (radius * 0.3, radius * 0.3))
 
     def set_cells_eaten_max(self, value=0):                                     # Change nb of cells player can eat
         self.cells_eaten_max = value
 
     def ChangeLevel(self):                                                      # Adapt player to next level
-        if self.cells_eaten >= self.cells_eaten_max:
-            self.evolution += 1
-            self.cells_eaten = 0
-            self.life = self.life_max
+        self.evolution += 1
+        self.cells_eaten = 0
+        self.Stomach = []
+        self.current_radius = int(self.max_radius * 0.5)
+        self.current_diameter = self.current_radius * 2
+        self.screen_pos = (int(self.screen_size[0] * 0.5 - self.current_radius), int(self.screen_size[1] * 0.5 - self.current_radius))
+        self.membrane_width = int(self.current_radius * 0.1)
         self.rect.x = self.set_position_x()
         self.rect.y = self.set_position_y()
+        self.set_image()
 
     def Eat(self):
         self.cells_eaten += 1
+        self.current_radius += self.cells_eaten / self.cells_eaten_max * (self.max_radius - self.current_radius)  # Grow
+        self.current_diameter = self.current_radius * 2
+        self.membrane_width = int(self.current_radius * 0.1)
+        self.screen_pos = (int(self.screen_size[0] * 0.5 - self.current_radius), int(self.screen_size[1] * 0.5 - self.current_radius))
+        self.set_image(self.current_radius)
+
+        if len(self.Stomach) < self.stomach_capacity:
+            self.Stomach.append(CellEaten(player=self))
+        for cell in self.Stomach:
+            cell.set_player_radius(self.current_radius)
 
     def Attack(self):                                                           # ! [later] Add damage, defense, skills...
         pass
 
+    def GainLife(self, amount=0):
+        self.life += amount
+        if self.life >= self.life_max:
+            self.life = self.life_max
+
+    def TakeDamage(self, amount=0):
+        self.life -= amount
+        if self.life <= 0:
+            self.life = 0
+            self.death_sound.play()
+            self.is_dead = True
+
     def ChangeMovementMethod(self):                                             # Change the movement type of the player
         self.move_method = self.Methods[0] if self.move_method == self.Methods[1] else self.Methods[1]
 
-    def MovementManager(self, map_borders, screen_size=None, pressed=None):     # Manage the movements in the game
+    # Manage the movements in the game
+    def MovementManager(self, map_borders, screen_size, pressed=None, Cells=None, LifeBubbles=None, Enemies=None):
         if self.move_method == self.Methods[0]:
             self.MoveWithMouse(screen_size)
         elif self.move_method == self.Methods[1]:
             self.MoveWithKeyboard(pressed)
 
         self.CheckBordersCollision(map_borders)
+        self.CheckCellsCollision(Cells)
+        self.CheckLifeBubblesCollision(LifeBubbles)
+        self.CheckEnemiesCollision(Enemies)
 
     def MoveWithMouse(self, screen_size=None, goal_pos=None):                   # Go to the mouse position
         if screen_size is None:
@@ -92,24 +132,26 @@ class Player(Bubble):                                                           
             goal_pos = pygame.mouse.get_pos()
 
         if 0 <= goal_pos[0] <= screen_size[0] and 0 <= goal_pos[1] <= screen_size[1]:
-            goal_x = goal_pos[0] - self.radius
-            goal_y = goal_pos[1] - self.radius
-            pos_x = screen_size[0] // 2 - self.radius
-            pos_y = screen_size[1] // 2 - self.radius
+            goal_x = goal_pos[0] - self.current_radius
+            goal_y = goal_pos[1] - self.current_radius
+            pos_x = screen_size[0] // 2 - self.current_radius
+            pos_y = screen_size[1] // 2 - self.current_radius
             dx = goal_x - pos_x                                                 # Calculate distance player-mouse
             dy = goal_y - pos_y
             distance = ((dx ** 2) + (dy ** 2)) ** 0.5
 
-            if distance <= self.radius:                                         # Mouse on player
+            if distance <= self.current_radius:                                 # Mouse on player
                 speed = 0
-            elif self.radius < distance < self.diameter * 1.5:                  # Make dynamic speed
-                speed = (distance - self.radius) / self.diameter * self.speed   # Percent x Speed
+            elif self.current_radius < distance < self.diameter * 1.5:          # Make dynamic speed
+                speed = (distance - self.current_radius) / self.diameter * self.speed   # Percent x Speed
             else:                                                               # Max speed
                 speed = self.speed
 
             if distance != 0:
-                self.rect.x += (dx / distance) * speed
-                self.rect.y += (dy / distance) * speed
+                self.dir_x = (dx / distance) * speed
+                self.dir_y = (dy / distance) * speed
+            self.rect.x += self.dir_x
+            self.rect.y += self.dir_y
 
     def MoveWithKeyboard(self, pressed=None):                                   # Move player with arrows
         if pressed is None:
@@ -126,6 +168,53 @@ class Player(Bubble):                                                           
             dir_x += 1
 
         distance = sqrt(dir_x ** 2 + dir_y ** 2)                                # Calculate distance to go
-        if distance != 0:
-            self.rect.x += (dir_x / distance) * self.speed                      # Update position
-            self.rect.y += (dir_y / distance) * self.speed
+        self.dir_x = (dir_x / distance) * self.speed if distance != 0 else 0
+        self.dir_y = (dir_y / distance) * self.speed if distance != 0 else 0
+        self.rect.x += self.dir_x
+        self.rect.y += self.dir_y
+
+    def check_touch_circle(self, circle):
+        return sqrt((circle.rect.x + circle.radius - self.rect.x - self.current_radius) ** 2 +
+                    (circle.rect.y + circle.radius - self.rect.y - self.current_radius) ** 2) <= self.current_radius - circle.radius
+
+    def CollideBubble(self, bubble):
+        distance = self.get_min_distance(self, bubble)
+        if distance <= int(bubble.radius * 0.5):                                # Prevent player from eating bubble
+            bubble.rect.x += self.current_radius if self.rect.x < bubble.rect.x else - self.current_radius
+            bubble.rect.y += self.current_radius if self.rect.y < bubble.rect.y else - self.current_radius
+
+    def CheckCellsCollision(self, Cells=None):                                  # Check if eat cells
+        if Cells is None:
+            return
+
+        for cell in Cells:
+            if self.check_touch_circle(cell):
+                self.Eat()
+                cell.SelfDestruction()
+                Cells.remove(cell)
+
+    def CheckLifeBubblesCollision(self, LifeBubbles=None):                      # Check if eat life-bubbles
+        if LifeBubbles is None:
+            return
+
+        for life_bubble in LifeBubbles:
+            if self.check_touch_circle(life_bubble):
+                if self.life < self.life_max:                                   # Prevent player from wasting healing
+                    self.GainLife(life_bubble.life_gain)
+                    life_bubble.SelfDestruction()
+                    LifeBubbles.remove(life_bubble)
+                else:
+                    self.CollideBubble(life_bubble)
+
+    def CheckEnemiesCollision(self, Enemies=None):                              # Check if can eat enemies
+        if Enemies is None:
+            return
+
+        for enemy in Enemies:
+            if self.check_touch_circle(enemy):
+                if self.current_radius >= enemy.radius * 1.5:                   # Prevent player from taking damage
+                    self.TakeDamage(enemy.damage)
+                    enemy.SelfDestruction()
+                    Enemies.remove(enemy)
+                else:
+                    self.CollideBubble(enemy)
--- testception.py
+++ testception.py
@@ -1,188 +1,76 @@
 import pygame
-from math import *
-from random import *
+import random
+import math
 
-""" Collision des bulles d'un même groupe """
-
+# Initialisation de Pygame
 pygame.init()
-window_width = 800
-window_height = 600
-screen = pygame.display.set_mode((window_width, window_height))
-pygame.display.set_caption("Bubble royale")
-clock = pygame.time.Clock()
+
+# Dimensions de la fenêtre
+width = 800
+height = 600
+
+# Création de la fenêtre
+window = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Cercles qui fuient")
 
 # Couleurs
-bg_color = (84, 210, 250)                                                       # Couleur de fond
-Colors = [(234, 48, 39), (254, 156, 39), (242, 242, 34), (169, 252, 41), (41, 232, 44), (41, 252, 217), (41, 217, 252),
-          (41, 124, 252), (36, 45, 238), (144, 46, 255), (204, 56, 252), (232, 56, 239), (196, 88, 22)]
+black = (0, 0, 0)
+white = (255, 255, 255)
 
+# Position et rayon du cercle qui bouge
+circle_x = random.randint(100, width-100)
+circle_y = random.randint(100, height-100)
+circle_radius = 50
 
-class Bubble(pygame.sprite.Sprite):
-    def __init__(self, x=None, y=None, size=None, dx=None, dy=None, color=None, show_dir=False, *groups):
-        super().__init__(*groups)
-        max_size = 50
-        self.show_dir = show_dir
-        self.diameter = size if size is not None else randint(int(max_size * 0.25), max_size)
-        self.radius = int(self.diameter * 0.5)
-        self.pos_x = x if x is not None else randint(self.radius, window_width - self.diameter)
-        self.pos_y = y if y is not None else randint(self.radius, window_height - self.diameter)
-        self.direction_x = dx if dx is not None else choice([-1, 1]) * randint(1, 3)
-        self.direction_y = dy if dy is not None else choice([-1, 1]) * randint(1, 3)
-        self.border_width = int(self.diameter * 0.05)
-        # Colors
-        self.color_transparent = 160
-        self.border_transparent = self.color_transparent + 40
-        self.reflect_transparent = self.color_transparent - 40
-        self.color = self.check_color(color)
-        self.border_color = self.set_border_color(self.color)
-        self.reflect_color = self.set_reflect_color(self.color)
-        self.arrow_color = self.reflect_color
-        # Bubble
-        self.bubble_surface = pygame.Surface((self.diameter, self.diameter), pygame.SRCALPHA)           # Surface of the bubble
-        pygame.draw.circle(self.bubble_surface, self.color, (self.radius, self.radius), self.radius)    # Bulle remplie
-        pygame.draw.circle(self.bubble_surface, self.border_color, (self.radius, self.radius), self.radius, self.border_width)  # Rebords de la bulle
-        # Reflect
-        self.reflect_surface = pygame.Surface((int(self.diameter * 0.12), int(self.diameter * 0.3)), pygame.SRCALPHA)
-        reflect_size = (0, 0, self.reflect_surface.get_width(), self.reflect_surface.get_height())
-        pygame.draw.ellipse(self.reflect_surface, self.reflect_color, reflect_size)                     # Dessine reflet
-        self.reflect_surface = pygame.transform.rotate(self.reflect_surface, -45)                       # Tourne surface
-        self.bubble_surface.blit(self.reflect_surface, (self.radius * 0.25, self.radius * 0.25))
+# Position et rayon du cercle fixe
+target_x = width // 2
+target_y = height // 2
+target_radius = 100
 
-    def check_color(self, checked_color):
-        checked_color = list(checked_color)
-        if len(checked_color) == 3:
-            checked_color = [*checked_color, self.color_transparent]
-        elif checked_color[3] != self.color_transparent:
-            checked_color[3] = self.color_transparent
-        return tuple(checked_color)
+# Vitesse de déplacement du cercle
+speed = 2
 
-    def set_border_color(self, border_color):
-        border_color = list(border_color)
-        if len(border_color) == 3:
-            border_color = [*border_color, self.border_transparent]
-        elif border_color[3] != self.border_transparent:
-            border_color[3] = self.border_transparent
-        for i in range(3):
-            border_color[i] = int(border_color[i] * 1.2)
-            border_color[i] = 255 if border_color[i] > 255 else border_color[i]
-        return tuple(border_color)
-
-    def set_reflect_color(self, reflect_color):
-        reflect_color = list(reflect_color)
-        if len(reflect_color) == 3:
-            reflect_color = [*reflect_color, self.reflect_transparent]
-        elif reflect_color[3] != self.reflect_transparent:
-            reflect_color[3] = self.reflect_transparent
-        for i in range(3):
-            reflect_color[i] = int(reflect_color[i] * 1.8)
-            reflect_color[i] = 255 if reflect_color[i] > 255 else reflect_color[i]
-        return tuple(reflect_color)
-
-    @staticmethod
-    def get_min_distance(circle1, circle2):
-        dx = (circle2.pos_x + circle2.direction_x) - (circle1.pos_x + circle1.direction_x)          # Calcule future pos
-        dy = (circle2.pos_y + circle2.direction_y) - (circle1.pos_y + circle1.direction_y)
-        return sqrt(dx ** 2 + dy ** 2) - circle1.radius - circle2.radius
-
-    def update(self, circles=None):
-        if circles is None:
-            circles = []
-
-        # Vérifie les collisions avec les autres cercles
-        for circle in circles:
-            distance = self.get_min_distance(self, circle)
-            if circle is not self:
-                if distance <= 0:
-                    dx = self.pos_x - circle.pos_x
-                    dy = self.pos_y - circle.pos_y
-                    angle = atan2(dy, dx)                                       # Direction vecteur entre centres cercles
-                    overlap = -distance / 2                                     # Distance de chevauchement entre cercles
-
-                    # Déplace les cercles pour les séparer
-                    self.pos_x += cos(angle) * overlap
-                    self.pos_y += sin(angle) * overlap
-                    self.check_screen_collision()
-                    circle.pos_x -= cos(angle) * overlap
-                    circle.pos_y -= sin(angle) * overlap
-                    circle.check_screen_collision()
-
-        # Met à jour la position
-        self.pos_x += self.direction_x
-        self.pos_y += self.direction_y
-        self.check_screen_collision()
-
-    def check_screen_collision(self):                                           # Vérifie collisions avec bords de l'écran
-        self.direction_x = 1 if self.pos_x - self.radius < 0 else -1 if self.pos_x + self.radius > window_width else self.direction_x
-        self.direction_y = 1 if self.pos_y - self.radius < 0 else -1 if self.pos_y + self.radius > window_height else self.direction_y
-
-    def draw(self):
-        screen.blit(self.bubble_surface, (self.pos_x - self.radius, self.pos_y - self.radius))
-
-        # reflect_surface_pos = (self.pos_x - self.radius * 0.75, self.pos_y - self.radius * 0.75)
-        # screen.blit(self.reflect_surface, reflect_surface_pos)                       # Dessine surface tournée avec reflet
-
-        if self.show_dir:
-            percent_gap_circle = 1.5
-            percent_arrow_width = 0.15
-            percent_arrow_height = 2
-            angle = atan2(self.direction_y, self.direction_x)                       # Direction cercle (en radian)
-            line_start = (self.pos_x + self.radius * 0.8 * cos(angle), self.pos_y + self.radius * 0.8 * sin(angle))
-            line_end = (self.pos_x + self.radius * percent_gap_circle * cos(angle), self.pos_y + self.radius * percent_gap_circle * sin(angle))
-            pygame.draw.line(screen, self.arrow_color, line_start, line_end, int(self.radius // 10))    # Tige flèche
-            point1 = (self.pos_x + self.radius * percent_gap_circle * cos(angle - percent_arrow_width),
-                      self.pos_y + self.radius * percent_gap_circle * sin(angle - percent_arrow_width))
-            point2 = (self.pos_x + self.radius * percent_gap_circle * cos(angle + percent_arrow_width),
-                      self.pos_y + self.radius * percent_gap_circle * sin(angle + percent_arrow_width))
-            point3 = (self.pos_x + self.radius * percent_arrow_height * cos(angle),
-                      self.pos_y + self.radius * percent_arrow_height * sin(angle))
-            pygame.draw.polygon(screen, self.arrow_color, [point1, point2, point3])
-
-
-# Créer des bulles
-objects = []
-nb_objet = 10 if len(Colors) <= 10 else len(Colors)
-for _ in range(nb_objet):
-    color = choice(Colors)
-    objects.append(Bubble(color=color))
-    Colors.remove(color)
-
+# Boucle principale
 running = True
-pausing = False
-fps = 100
-old_pfs = fps
-print(f"{fps}fps")
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
-        if event.type == pygame.KEYUP:
-            if event.key == pygame.K_SPACE:
-                pausing = not pausing
+    # Déplacement du cercle de manière aléatoire
+    circle_x += random.randint(-speed, speed)
+    circle_y += random.randint(-speed, speed)
 
-            if event.key == pygame.K_j:
-                fps -= 10
-                old_pfs = fps
-                print(f"{fps}fps")
-            elif event.key == pygame.K_l:
-                fps += 10
-                old_pfs = fps
-                print(f"{fps}fps")
+    # Vérifier les limites de l'écran pour le cercle
+    if circle_x - circle_radius < 0:
+        circle_x = circle_radius
+    elif circle_x + circle_radius > width:
+        circle_x = width - circle_radius
+    if circle_y - circle_radius < 0:
+        circle_y = circle_radius
+    elif circle_y + circle_radius > height:
+        circle_y = height - circle_radius
 
-            if event.key == pygame.K_q:
-                running = False
+    # Calculer la distance entre les cercles
+    distance = math.sqrt((circle_x - target_x)**2 + (circle_y - target_y)**2)
 
-    pressed = pygame.key.get_pressed()
-    fps = 20 if pressed[pygame.K_k] else old_pfs
+    # Vérifier si les cercles se rapprochent trop
+    if distance < target_radius + circle_radius:
+        # Inverser la direction du cercle
+        circle_x -= 2 * (circle_x - target_x)
+        circle_y -= 2 * (circle_y - target_y)
 
-    screen.fill(bg_color)
+    # Effacer l'écran
+    window.fill(black)
 
-    for obj in objects:                                                         # Met à jour et dessine tous les objets
-        if not pausing:
-            obj.update(objects)
-        obj.draw()
+    # Tracer le cercle qui bouge
+    pygame.draw.circle(window, white, (circle_x, circle_y), circle_radius)
 
+    # Tracer le cercle fixe
+    pygame.draw.circle(window, white, (target_x, target_y), target_radius, 2)
+
+    # Mettre à jour l'affichage
     pygame.display.flip()
-    clock.tick(fps)
 
+# Quitter Pygame
 pygame.quit()
